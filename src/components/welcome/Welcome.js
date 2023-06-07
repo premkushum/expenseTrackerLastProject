526284164054d1e69dfe8295082864fe786f85c8
shivam5676 <ssultanpura9@gmail.com>
2023-06-06 22:08:55 -0700

    verify email id button added in welcome page it will verify user by sending a mail

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,17 +1,44 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const Welcome = () => {
+  const verifyEmailHandler = () => {
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCKcB2kOdfnhOnnC789pc1BZXjZzebQnu0",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          requestType: "VERIFY_EMAIL",
+          idToken: localStorage.getItem("token"),
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("not verified");
+        }
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
-      <div style={{ display:"inline-flex" ,marginRight:"40%"}}>
+      <div style={{ display: "inline-flex", marginRight: "40%" }}>
         <h2>welcome to Expense tracker</h2>
       </div>
-      <div style={{textAlign:"right", display:"inline-flex"}}>
+      <div style={{ textAlign: "right", display: "inline-flex" }}>
         <h2>
           your profile is incomplete <Link to="/profile">complete now</Link>
         </h2>
       </div>
       <hr></hr>
+      <button onClick={verifyEmailHandler}>verify email</button>
     </div>
   );
 };
