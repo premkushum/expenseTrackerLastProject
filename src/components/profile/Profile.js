bc92be3fc26f31e5b8e7bb3da7a9171ca7dea90e
shivam5676 <ssultanpura9@gmail.com>
2023-06-06 21:41:03 -0700

    profile will get input fields automatically from firebase once user logged in

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,25 +1,53 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Profile = () => {
   const fullNameRef = useRef();
   const urlRef = useRef();
+  const [intialName, setIntialName] = useState("");
+  const [intialUrl, setIntialUrl] = useState("");
+
+  useEffect(() => {
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCKcB2kOdfnhOnnC789pc1BZXjZzebQnu0",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: localStorage.getItem("token"),
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Authentication error");
+        }
+      })
+      .then((res) => {
+        console.log(res.users[0].displayName);
+        setIntialName(res.users[0].displayName);
+        setIntialUrl(res.users[0].photoUrl);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const updateFormDataHandler = (event) => {
     event.preventDefault();
     const nameRefValue = fullNameRef.current.value;
     const urlRefValue = urlRef.current.value;
     const localstr = localStorage.getItem("token");
-    console.log(localstr);
 
     fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCKcB2kOdfnhOnnC789pc1BZXjZzebQnu0 ",
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCKcB2kOdfnhOnnC789pc1BZXjZzebQnu0",
       {
         method: "POST",
         body: JSON.stringify({
           idToken: localstr,
           displayName: nameRefValue,
           photoUrl: urlRefValue,
-      
           returnSecureToken: true,
         }),
         headers: { "Content-Type": "application/json" },
@@ -40,20 +68,24 @@ const Profile = () => {
       });
   };
 
+ 
+  
+
   return (
-    <form onSubmit= {updateFormDataHandler}>
+    <form onSubmit={updateFormDataHandler}>
       <div>
         <h2>Contact details</h2>
       </div>
 
       <label>Full Name</label>
-      <input type="text" ref={fullNameRef}></input>
+      <input type="text" ref={fullNameRef} defaultValue={intialName} />
 
       <label>Profile Photo URL</label>
-      <input type="text" ref={urlRef}></input>
+      <input type="text" ref={urlRef} defaultValue={intialUrl} />
 
-      <button >UPDATE</button>
+      <button>UPDATE</button>
     </form>
   );
 };
+
 export default Profile;
