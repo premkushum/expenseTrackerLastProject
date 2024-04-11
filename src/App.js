1b8d0f68882950c93267bb7ac603c6d1713843b4
premkushum <155063863+premkushum@users.noreply.github.com>
2024-04-11 10:16:22 +0530

    all changes

--- src/App.js
+++ src/App.js
@@ -16,56 +16,7 @@ import ExpenseContext from "./store/ExpenseContext";
 import { useState } from "react";
 
 function App() {
-  // const themeLight = useSelector((state) => state.theme.isLight);
-  // const [themelight, settheme] = useState(true);
-  // const dispatch = useDispatch();
 
-  // const expenseCtx = useContext(ExpenseContext);
-  // const initialEmail = localStorage.getItem("email");
-  // const [testing, setTesting] = useState(
-  //   initialEmail ? initialEmail.replace("@", "").replace(".", "") : false
-  // );
-
-  // console.log(initialEmail);
-
-  // useEffect(() => {
-  //   fetch(
-  //     `https://expensetrackerdemo-4954a-default-rtdb.firebaseio.com/${testing}.json`,
-  //     {
-  //       method: "GET",
-  //       headers: { "Content-Type": "application/json" },
-  //     }
-  //   )
-  //     .then((response) => {
-  //       if (response.ok) {
-  //         return response.json();
-  //       } else {
-  //         throw new Error("data didnt save in server");
-  //       }
-  //     })
-
-  //     .then((res) => {
-  //       if (res) {
-  //         console.log(res);
-  //         const newItems = Object.values(res).map((item) => {
-  //           const myobj = {
-  //             key: item.token,
-  //             id: item.id,
-  //             name: item.name,
-  //             amount: item.amount,
-  //             category: item.category,
-  //             token: item.token,
-  //             date: item.date,
-  //           };
-  //           console.log(myobj);
-  //           expenseCtx.addExpense(myobj);
-  //         });
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       toast.error("data did not load");
-  //     });
-  // }, [testing]);
   return (
     <>
       <Switch>
